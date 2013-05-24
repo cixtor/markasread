a345b72ad78ae1fe459406b53fa2b8bec4de9715
Cixtor <cixtords@gmail.com>
2013-05-24 09:00:00 -0500

    Added event handler to add and delete URLs from the history

--- background.js
+++ background.js
@@ -14,13 +14,7 @@
  * so on.
  */
 
-chrome.browserAction.onClicked.addListener(function (tab) {
-	chrome.tabs.executeScript(null, {
-		code: 'markAsRead();'
-	});
-});
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+var processHistoryAction = function (request, sender) {
 	var urls = request.urls;
 	var total = urls.length;
 
@@ -37,5 +31,45 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 		} else {
 			chrome.history.addUrl(url);
 		}
+	}
+};
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+	chrome.tabs.executeScript(tab.id, {
+		'code': 'markAsRead();'
+	});
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender) {
+	/**
+	 * Handle the history manipulation.
+	 *
+	 * If the user clicks the browser action button for the first time the
+	 * extension will scan the active page for important links, this is, any
+	 * HTML anchor tag listed in news aggregator websites and that lead to
+	 * either an internal or external content of the associated article.
+	 *
+	 * @param  {string} request.scope Action that is being performed.
+	 * @return {void}
+	 */
+	if (request.scope === 'history') {
+		processHistoryAction(request, sender);
+		setBadgeBackground(request, sender);
+	}
+
+	/**
+	 * Display number of important links.
+	 *
+	 * Once a supported website is loaded the extension will scan the active
+	 * page for important links, then will try to analyze the history to see if
+	 * one of more of these links are already marked as visited, the remaining
+	 * URLs will be part of a counter in the badge of the browser action.
+	 *
+	 * @param  {string} request.scope Action that is being performed.
+	 * @return {void}
+	 */
+	if (request.scope === 'interface') {
+		processInterfaceAction(request, sender);
+		setBadgeBackground(request, sender);
 	}
 });
